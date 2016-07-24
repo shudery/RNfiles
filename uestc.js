14eacc3ed4df8235e108e35c644cb8306d72c541
uclinxiuda <wb-lxd188077@alibaba-inc.com>
2016-07-24 17:59:36 +0800

    uestcapp

--- uestc.js
+++ uestc.js
@@ -17,7 +17,7 @@ import {
   View
 } from 'react-native';
 
-var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+var REQUEST_URL = 'http://uestc-home-json.herokuapp.com/'
 
 class firstRN extends React.Component {
   constructor(props) {
@@ -45,7 +45,7 @@ class firstRN extends React.Component {
         // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
         this.setState({
           //ListView必须字段
-          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          dataSource: this.state.dataSource.cloneWithRows(responseData),
           loaded: true,
         });
       })
@@ -59,65 +59,59 @@ class firstRN extends React.Component {
     }
 
     return (
+      <View>
+       <Image
+        style={styles.logo}
+        source={{uri: 'http://www.uestc.edu.cn/images/logo.png'}}/>
       <ListView
         dataSource={this.state.dataSource}
         renderRow={this.renderMovie}
         style={styles.listView}/>
+       </View> 
     );
   }
   renderLoadingView() {
     return (
-      <View style={styles.container}>
-        <Text>
-          正在加载电影数据……
+      <View>
+        <Text style={styles.loading}>
+          正在加载uestc官网数据……
         </Text>
       </View>
     )
   }
 
-  renderMovie(movie) {
+  renderMovie(data) {
     return (
+
       <View style={styles.container}>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}/>
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
-        </View>
+          <Text style={styles.title}>{data.text}</Text>
+          <Text style={styles.href} href={styles.href}>{data.href}</Text>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  loading:{
+    marginTop:300,
+    textAlign:'center',
+  },
+  href:{
+    color:'blue',
+  },
+  logo:{
+    margin:10,
+    height:100,
+  },
    listView: {
     paddingTop: 20,
     backgroundColor: '#F5FCFF',
   },
-  thumbnail:{
-    width:53,
-    height:110,
-  },
   container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    margin:10,
     backgroundColor: '#F5FCFF',
   },
-    
-  rightContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
+ 
 });
 
 AppRegistry.registerComponent('firstRN', () => firstRN);
